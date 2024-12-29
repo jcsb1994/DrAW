9446974a4faa40b9e90c694a2c5b3f5d438387e2
JCSB <jeanchristophesicottebrisson@gmail.com>
2024-12-28 21:14:37 -0500

    Main component displays as a pink rect

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,6 +15,7 @@ Juce_sandboxAudioProcessorEditor::Juce_sandboxAudioProcessorEditor (Juce_sandbox
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
+    addAndMakeVisible(main_cmp);
     setSize (400, 300);
 }
 
@@ -37,4 +38,5 @@ void Juce_sandboxAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    main_cmp.setBounds(3, 3, 350, 250);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -10,6 +10,7 @@
 
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
+#include "MainCmp.h"
 
 //==============================================================================
 /**
@@ -28,6 +29,7 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     Juce_sandboxAudioProcessor& audioProcessor;
+    MainCmp main_cmp;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Juce_sandboxAudioProcessorEditor)
 };
